6f27981283e48dfa67ae0bdbe7ab4f2b91b9bcad
AnuragRajput-cyber <anuragrajput1226@gmail.com>
2025-01-20 23:36:59 +0530

    Show images on Gallery Tab

--- cloudinary-photos/src/app/gallery/page.tsx
+++ cloudinary-photos/src/app/gallery/page.tsx
@@ -1,36 +1,38 @@
-"use client";
-import { CldUploadButton } from "next-cloudinary";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
-export default function GalleryPage() {
+import cloudinary from 'cloudinary';
+import UploadButton from "./upload-button";
+import { CldImage } from 'next-cloudinary';
+import { CloudinaryImage } from './cloudinary-image';
+
+type searchResult ={
+  public_id:string
+}
+
+export default async function GalleryPage() {
+  const results = await cloudinary.v2.search
+  .expression('resource_type:image')
+  .sort_by('created_at','desc')
+  .max_results(10)
+  .execute() as {resources:searchResult[]};
+
   return (
-    <section className="flex justify-between">
+    <section>
+      <div className='flex flex-col gap-8'>
+      <div className="flex justify-between">
       <h1 className="text-4xl font-bold">Gallery</h1>
-      <Button asChild>
-        <div className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-            />
-          </svg>
-        
-        <CldUploadButton
-          onSuccess={(result: UploadResult | any) => {
-            // setImageId(result.info.public_id);
-          }}
-          uploadPreset="upload_preset"
-        ></CldUploadButton>
-        </div>
-      </Button>
+      <UploadButton/>
+      </div>
+<div className='grid grid-cols-4 gap-4'>
+      {results.resources.map((result)=>(
+        <CloudinaryImage
+        key={result.public_id}
+        src={result.public_id}
+        width="400"
+        height="300"
+        alt="Description of an image"
+        />
+      ))}
+</div>
+      </div>
     </section>
   );
-}
+};
